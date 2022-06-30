4c5bd93f89bf63fd73ead966acc4c2f5b6fc82e4
EdvinasStankunas <98836185+EdvinasStankunas@users.noreply.github.com>
2022-06-30 18:51:03 +0300

    Use alternative solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,26 @@
+//This line declares function that can accept multiple arguments and puts them in an array
 const removeFromArray = function (...values) {
-  /* This line declares function that can accept multiple arguments and puts them in an array */
-  const array =
-    values.shift(); /*Since the first passed value in values[] is an array that needs to be compared to values array, this needs to take it out of the values array */
-  const newArray = []; /* New array that is going to contain wanted values */
+  //Since the first passed value in values[] is an array that needs to be compared to values array, this puts the first passed value into it's own array
+  //Lack of caffeine made me anxious about first value (array) passing into filteredArray, but now I see the obvious.
+  const array = values[0];
+  //This line declares a new array which is going to receive the elements that return false value after .includes() method runs
+  const filteredArray = array.filter((item) => !values.includes(item));
+
+  /* MY FIRST SOLUTION:
+  
+  New array that is going to contain wanted values
+  const filteredArray = [];
+  // scan through all array[] items
   array.forEach((item) => {
-    /* scan through all array[] items */
+    // if values[] does not include current item from array[]
     if (!values.includes(item)) {
-      /* if values[] does not include current item from array[]*/
-      newArray.push(item); /*push said item into a newArray[] */
+      //push said item into filteredArray[]
+      filteredArray.push(item);
     }
   });
-  return newArray; /* Once the loop is finished, function returns newArray[] populated with wanted values. */
+  // Once the loop is finished, function returns filteredArray[] populated with wanted values. */
+
+  return filteredArray;
 };
 // Do not edit below this line
 module.exports = removeFromArray;
