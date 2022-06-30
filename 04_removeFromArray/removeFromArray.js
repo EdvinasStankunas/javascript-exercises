37aae92d5e538c92bcec567947a9b832440195e5
EdvinasStankunas <98836185+EdvinasStankunas@users.noreply.github.com>
2022-06-30 12:42:17 +0300

    Add comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,16 @@
 const removeFromArray = function (...values) {
-  const array = values.shift();
-  const newArray = [];
+  /* This line declares function that can accept multiple arguments and puts them in an array */
+  const array =
+    values.shift(); /*Since the first passed value in values[] is an array that needs to be compared to values array, this needs to take it out of the values array */
+  const newArray = []; /* New array that is going to contain wanted values */
   array.forEach((item) => {
+    /* scan through all array[] items */
     if (!values.includes(item)) {
-      newArray.push(item);
+      /* if values[] does not include current item from array[]*/
+      newArray.push(item); /*push said item into a newArray[] */
     }
   });
-  return newArray;
+  return newArray; /* Once the loop is finished, function returns newArray[] populated with wanted values. */
 };
 // Do not edit below this line
 module.exports = removeFromArray;
