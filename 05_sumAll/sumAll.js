82e4916b8d880810158c4e3c9d57715bf0871b2c
EdvinasStankunas <98836185+EdvinasStankunas@users.noreply.github.com>
2022-07-01 12:14:52 +0300

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (firstArg, secArg) {
+  let sum = 0;
+  if (
+    typeof firstArg != "number" ||
+    typeof secArg != "number" ||
+    firstArg <= 0 ||
+    secArg <= 0
+  ) {
+    return "ERROR";
+  }
+  if (firstArg > secArg) {
+    const temp = firstArg;
+    firstArg = secArg;
+    secArg = temp;
+  }
+  for (let x = firstArg; x < secArg + 1; x++) {
+    sum = sum + x;
+  }
+  return sum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
