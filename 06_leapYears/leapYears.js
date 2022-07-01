25ddc1ce34b421f5abb66a42994026701a341b30
EdvinasStankunas <98836185+EdvinasStankunas@users.noreply.github.com>
2022-07-01 12:42:31 +0300

    Solve 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (largeYear) {
+  if (largeYear % 4 == 0 && largeYear % 100 != 0) {
+    return true;
+  } else {
+    if (largeYear % 400 == 0) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Do not edit below this line
