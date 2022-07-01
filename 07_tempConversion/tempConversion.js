022b68f8600751ddeaeb60a57de23c9fdae00825
EdvinasStankunas <98836185+EdvinasStankunas@users.noreply.github.com>
2022-07-01 14:39:20 +0300

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (far) {
+  let tempC = (far - 32) * (5 / 9);
+  tempC = Math.round(tempC * 10) / 10;
+  return tempC;
 };
 
-const ctof = function() {
-
+const ctof = function (cel) {
+  let tempF = cel * (9 / 5) + 32;
+  tempF = Math.round(tempF * 10) / 10;
+  return tempF;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
