9435a85224959ae6c85aeb3b2635e9c5d2523b7e
EdvinasStankunas <98836185+EdvinasStankunas@users.noreply.github.com>
2022-07-04 11:40:51 +0300

    Change variable names for readability

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
-const ftoc = function (far) {
-  let tempC = (far - 32) * (5 / 9);
+const ftoc = function (tempF) {
+  let tempC = (tempF - 32) * (5 / 9);
   tempC = Math.round(tempC * 10) / 10;
   return tempC;
 };
 
-const ctof = function (cel) {
-  let tempF = cel * (9 / 5) + 32;
+const ctof = function (tempC) {
+  let tempF = tempC * (9 / 5) + 32;
   tempF = Math.round(tempF * 10) / 10;
   return tempF;
 };
